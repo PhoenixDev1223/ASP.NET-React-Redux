3b82eba46d97a504fe66498d6810f42abbfbf02b
Full Stack Developer <phoenixdev1223@gmail.com>
2023-02-03 04:52:30 +0800

    MongoDB Config

---
+++ FirstWeb/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstWeb.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public string ConnectionSetting { get; set; } = null;
+        public string DatabaseName { get; set; } = null;
+        public string BooksCollectionName { get; set; } = null;
+    }
+}
--- FirstWeb/Startup.cs
+++ FirstWeb/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using FirstWeb.Models;
 
 namespace FirstWeb
 {
@@ -21,7 +22,7 @@ namespace FirstWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            services.Configure<BookStoreDatabaseSettings>(Configuration.GetSection("BookStoreDatabase"));
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
